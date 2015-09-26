b19a080c31a46a56e7eeabd33b847dc7c64c544c
Sterling Mortensen <mortymort5@aggiemail.usu.edu>
2015-09-26 17:48:59 -0600

    The profile page all most done

--- milestone/myScript.js
+++ milestone/myScript.js
@@ -14,3 +14,11 @@
     })
 
 })
+
+function validateForm() {
+    var x = document.forms["SM-myForm"]["SM-mForm"].value;
+    if (x == null || x == "") {
+        alert("All the input fields with the * need to be filled out!");
+        return false;
+    }
+}
