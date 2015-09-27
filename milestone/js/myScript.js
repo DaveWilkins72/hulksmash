8c1f63ab446b8741c428cf9f830976823909717c
Jordan Robertson <j_dog1991@hotmail.com>
2015-09-27 17:05:17 -0600

    I fixed up the code a bit, i'm still working on the javascript of dropdown menu.

--- milestone/js/myScript.js
+++ milestone/js/myScript.js
@@ -1,20 +1,3 @@
-(function(){
-  var documentElem = $(document),
-    header = $('head'),
-    lastScrollTop = 0;
-
-    documentElem.on('scroll', function(){
-      var currentScrollTop = $(this).scrollTop();
-
-      if(currentScrollTop > lastScrollTop ) header.addClass('hidden');
-
-      else header.removeClass('hidden');
-
-      lastScrollTop = currentScrollTop
-    })
-
-})
-
 function validateForm() {
     var x = document.forms["SM-myForm"]["SM-mForm"].value;
     if (x == null || x == "") {
