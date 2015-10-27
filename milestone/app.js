78fd9bf43fd4f604e634aa24d5df758915b96ae3
Jordan Robertson <j_dog1991@hotmail.com>
2015-10-26 22:30:21 -0600

    Added request to app.js, so do npm install on your terminal

--- milestone/app.js
+++ milestone/app.js
@@ -1,39 +1,39 @@
-var express =  require("express")
-var path = require("path")
+var express =  require("express");
+var exphbs = require('express-handlebars');
+var request = require('request');
+var path = require("path");
+
+var app = express();
 var PORT = 3000
-var exphbs = require('express-handlebars')
-
-var app = express()
-
 
 app.engine('handlebars', exphbs({defaultLayout: 'base'}));
 app.set('view engine', 'handlebars');
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req, res){
 
   res.render('home', {})
-})
+});
 
 app.get("/dashboard", function(req, res){
 
   res.render('dashboard', {})
-})
+});
 
 app.get("/profile", function(req, res){
 
   res.render('profile', {})
-})
+});
 
 app.get("/search", function(req, res){
 
   res.render('search', {})
-})
+});
 
 app.get("/savedSearch", function(req, res){
 
   res.render('savedSearch', {})
-})
+});
 
-app.listen(PORT)
+app.listen(PORT);
