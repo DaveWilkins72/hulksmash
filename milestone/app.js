2538b704bf8f3b406beac5dd977d697b69a40f6d
Jordan Robertson <j_dog1991@hotmail.com>
2015-11-04 20:09:48 -0700

    Authorization now works, please try and make sure it allows you to sign in

--- milestone/app.js
+++ milestone/app.js
@@ -2,20 +2,61 @@ var express =  require("express");
 var exphbs = require('express-handlebars');
 var request = require('request');
 var path = require("path");
+var querystring = require('querystring')
 
 var app = express();
-var ACCESS_TOKEN='your access token here';
-var PORT = 3000
+var PORT = 3000;
+
+var ACCESS_TOKEN='';
+var CLIENT_ID = 'fec535d5d4744523b272cde10c533caa'
+var CLIENT_SECRET = '7b0e709d6ec7426ba0a67a2184a4452f'
+var REDIRECT_URI = 'http://127.0.0.1:3000/auth/finalize'
 
 app.engine('handlebars', exphbs({defaultLayout: 'base'}));
 app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.get("/", function(req, res){
   ACCESS_TOKEN = ''
   res.render('home', {layout: 'homepage'})
 });
+
+app.get('/authorize', function(req, res){
+  var qs = {
+    client_id: CLIENT_ID,
+    redirect_uri: REDIRECT_URI,
+    response_type: 'code'
+  }
+
+  var query = querystring.stringify(qs)
+
+  var url = 'https://api.instagram.com/oauth/authorize/?' + query
+
+  res.redirect(url)
+})
+
+app.get('/auth/finalize', function(req, res){
+var post_data = {
+  client_id: CLIENT_ID,
+  client_secret: CLIENT_SECRET,
+  redirect_uri: REDIRECT_URI,
+  grant_type: 'authorization_code',
+  code: req.query.code
+}
+
+  var options = {
+    url: 'https://api.instagram.com/oauth/access_token',
+    form: post_data
+  }
+
+  request.post(options, function(error, response, body){
+    var data = JSON.parse(body)
+    ACCESS_TOKEN = data.access_token
+    res.redirect('/dashboard')
+  })
+})
 
 app.get("/dashboard", function(req, res){
   var options = {
